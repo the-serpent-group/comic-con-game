12e55a9d24904ece69a2c05a132207716bcfba2a
/estio <estio0011@gmail.com>
2024-03-21 09:33:08 +0000

    Add basic UI functionality. Needs more work. \n Also added comments to some places. \n Add TextMeshPro assets.

---
+++ Assets/Scripts/Managers/DebugManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+
+    [SerializeField] // This is here so that there is no need to enable debug mode via Konami Code in editor.
+    private bool isDebugEnabled = false;
+
+    void Update()
+    {
+        if (isDebugEnabled == true)
+        {
+            // Add Future Debug Code Here   ||
+            //                              \/
+            if (Input.GetKeyDown(KeyCode.Keypad9))
+            {
+                GameObject.Find("LevelManager").GetComponent<LevelManager>().ProgressDay();
+            }
+        }
+    }
+}
