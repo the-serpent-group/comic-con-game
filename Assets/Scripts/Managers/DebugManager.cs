24be79aeb37503ebb821761a96d0fc9314fcac51
Ernest <w21024244@northumbria.ac.uk>
2024-04-06 14:25:35 +0100

    Initial commit with all project files

---
+++ Assets/Scripts/Managers/DebugManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+
+    GridManager gridManager;
+    LevelManager levelManager;
+
+    [SerializeField] // This is here so that there is no need to enable debug mode via Konami Code in editor.
+    private bool isDebugEnabled = false;
+
+    private void Start()
+    {
+        gridManager = GameObject.Find("GridManager").GetComponent<GridManager>();
+        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+    }
+
+    void Update()
+    {
+        if (isDebugEnabled == true)
+        {
+            // Add Future Debug Code Here   ||
+            //                              \/
+            if (Input.GetKeyDown(KeyCode.Keypad9)) levelManager.ProgressDay();
+            if (Input.GetKeyDown(KeyCode.KeypadPlus)) gridManager.upgradeMap();
+            if (Input.GetKeyDown(KeyCode.KeypadMinus)) gridManager.downgradeMap();
+        }
+    }
+
+    public GameObject Debug_Killer;
+    public GameObject Debug_Spawner;
+
+    private void FixedUpdate()
+    {
+        GameObject Debug_Killer_Ref = GameObject.Find("Debug_Killer(Clone)");
+        if (isDebugEnabled == true)
+        {
+            if (!Debug_Killer_Ref) Instantiate(Debug_Killer).transform.position = gridManager.killerOffsetPosition;
+            else Debug_Killer_Ref.transform.position = gridManager.killerOffsetPosition;
+        }
+        if (isDebugEnabled != true)
+        {
+            if (Debug_Killer_Ref != null) Destroy(GameObject.Find("Debug_Killer(Clone)"));
+        }
+
+        GameObject Debug_Spawner_Ref = GameObject.Find("Debug_Spawner(Clone)");
+        if (isDebugEnabled == true)
+        {
+            if (!Debug_Spawner_Ref) Instantiate(Debug_Spawner).transform.position = gridManager.spawnerOffsetPosition;
+            else Debug_Spawner_Ref.transform.position = gridManager.spawnerOffsetPosition;
+        }
+        if (isDebugEnabled != true)
+        {
+            if (Debug_Spawner_Ref != null) Destroy(GameObject.Find("Debug_Spawner(Clone)"));
+        }
+    }
+}
---
+++ Assets/Scripts/StandValidator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class StandValidator : MonoBehaviour
+{
+    [SerializeField]
+    private string locationString = null;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public string generateLocationString()
+    {
+        Vector3 pos = transform.position;
+        pos.x = (int)pos.x;
+        pos.y = (int)pos.y;
+        pos.z = (int)pos.z;
+        locationString = $"x[{pos.x}]&y[{pos.y}]&z[{pos.z}]";
+        return locationString;
+    }
+
+    public bool validateStand()
+    {
+        generateLocationString();
+        StandManager standManager = GameObject.Find("Stand Manager").GetComponent<StandManager>();
+        if (standManager.activeStandList.Find(e => e == locationString) != null) return false;
+        return true;
+    }
+
+}
+
